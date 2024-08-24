a2f51b163e30c94e56fc558b60c7f53a1aea0338
JeonHaeseung <414catherine@gmail.com>
2024-08-24 22:09:52 +0900

    [test] Test code for CPU monitoring & dashboard

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pd
 import time
 
+from common.imports import *
 from monitoring.gpu import GpuCollector
 
 st.set_page_config(
@@ -11,17 +11,14 @@ st.set_page_config(
 
 st.title("Monitoring Dashboard")
 
-st.subheader("GPU")
-
-gpu_usage_data = []
 gpu_collector = GpuCollector()
-gpu_info = gpu_collector.gpu_info
+gpu_usage_data = []
 
+chart_placeholder = st.empty()
 
-df = pd.DataFrame(list(gpu_info.items()), columns=['GPU Number', 'Utilization (%)'])
-df['Timestamp'] = pd.Timestamp.now()
-df.set_index('Timestamp', inplace=True)
-gpu_usage_data.append(df)
-df_combined = pd.concat(gpu_usage_data)
-st.subheader("GPU Usage")
-st.area_chart(df_combined.pivot_table(index=df_combined.index, columns='GPU Number', values='Utilization (%)'))
+while True:
+    df = gpu_collector.gpu_info
+    gpu_usage_data.append(df)
+    df_combined = pd.concat(gpu_usage_data)
+    chart_placeholder.line_chart(df_combined.pivot_table(index=df_combined.index, columns='GPU Number', values='Utilization (%)'))
+    time.sleep(1)  # 1초 간격으로 데이터 수집
--- monitoring/gpu.py
+++ monitoring/gpu.py
@@ -1,12 +1,16 @@
 import pynvml
 
+from common.imports import *
+
 class GpuCollector(object):
     def __init__(self):
         pynvml.nvmlInit()
 
     @property
     def gpu_info(self):
-        gpu_usage = {}
+        timestamp = pd.Timestamp.now()
+
+        gpu_usage = []
 
         # Number of the GPUs
         num_gpu_cores = pynvml.nvmlDeviceGetCount()
@@ -15,9 +19,12 @@ class GpuCollector(object):
         for i in range(num_gpu_cores):
             handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             gpu_percent = pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
-            gpu_usage[i] = gpu_percent
+            gpu_usage.append({'GPU Number': i, 'Utilization (%)': gpu_percent, 'Timestamp': timestamp})
         
-        return gpu_usage
+        df = pd.DataFrame(gpu_usage)
+        df.set_index('Timestamp', inplace=True)
+
+        return df
 
     def __del__(self):
         pynvml.nvmlShutdown()
