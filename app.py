d890dc1e590b23ccbc4dc4ca515b019c07fa7dce
JeonHaeseung <414catherine@gmail.com>
2024-08-26 19:31:41 +0900

    [feat] Added GPU process usage / username / commond display feature

--- app.py
+++ app.py
@@ -15,23 +15,32 @@ st.title("Monitoring Dashboard")
 
 config_manager = ConfigManager()
 gpu_collector = GpuCollector()
-gpu_placeholder = st.empty()
+gpu_util_placeholder = st.empty()
+gpu_process_placeholder = st.empty()
 
 
-def gpu_charts():
-    gpu_data = gpu_collector.get_info
+def gpu_util_charts():
+    gpu_util_data = gpu_collector.gpu_util
     
-    chart = alt.Chart(gpu_data).mark_line().encode(
+    chart = alt.Chart(gpu_util_data).mark_line().encode(
         x=alt.X('Timestamp:O', title='Timestamp', axis=alt.Axis(labelAngle=0)),
         y=alt.Y('Percentage:Q', title='Percentage', scale=alt.Scale(domain=[0, 100])),
-        color=alt.Color('GPU Number:N', scale=gpu_collector.get_color)
+        color=alt.Color('GPU:N', scale=gpu_collector.color)
     ).properties(
         title='GPU Usage'
     )
 
-    gpu_placeholder.altair_chart(chart, use_container_width=True)
+    gpu_util_placeholder.altair_chart(chart, use_container_width=True)
+
+
+def gpu_process_charts():
+    gpu_process_data = gpu_collector.gpu_process
+    gpu_process_data = gpu_process_data.sort_values(by='GPU Memory (MB)', ascending=False)
+
+    gpu_process_placeholder.dataframe(gpu_process_data, hide_index=True)
 
 
 while True:
-    gpu_charts()
+    gpu_util_charts()
+    gpu_process_charts()
     time.sleep(config_manager.delta_second)
--- monitoring/gpu.py
+++ monitoring/gpu.py
@@ -1,4 +1,7 @@
+import os
+import pwd
 import pynvml
+import psutil
 
 from common.imports import *
 from common.time import *
@@ -8,23 +11,30 @@ from common.config import *
 class GpuCollector(object):
     def __init__(self):
         pynvml.nvmlInit()
-        self.data = pd.DataFrame(columns=['Timestamp', 'GPU Number', 'Percentage'])
-        self.config_manager = ConfigManager()
-        self.time_manager = TimeManager()
-        self.data_count  = self.config_manager.gpu_total_second // self.config_manager.gpu_delta_second
         self.gpu_num = pynvml.nvmlDeviceGetCount()
 
+        self.config_manager = ConfigManager()
+        self.time_manager = TimeManager()
 
-    def update_info(self, new):
-        self.data = pd.concat([self.data, new], ignore_index=True)
+        self.gpu_util_data = pd.DataFrame(columns=['Timestamp', 'GPU', 'Percentage'])
+        self.gpu_util_data_count  = self.config_manager.gpu_total_second // self.config_manager.gpu_delta_second
+        self.gpu_process_data = pd.DataFrame(columns=['GPU Memory (MB)', 'GPU', 'PID', 'User', 'Command'])
+        
 
-        if len(self.data) // self.gpu_num > self.data_count:
-            second = len(self.data) // self.gpu_num - self.data_count
-            self.data = self.data.iloc[second*self.gpu_num:]
+    def update_gpu_util_data(self, new):
+        self.gpu_util_data = pd.concat([self.gpu_util_data, new], ignore_index=True)
+
+        if len(self.gpu_util_data) // self.gpu_num > self.gpu_util_data_count:
+            second = len(self.gpu_util_data) // self.gpu_num - self.gpu_util_data_count
+            self.gpu_util_data = self.gpu_util_data.iloc[second*self.gpu_num:]
+
+
+    def update_gpu_process_data(self, new):
+        self.gpu_process_data = new
 
 
     @property
-    def get_info(self):
+    def gpu_util(self):
         timestamp = self.time_manager.get_timestamp
 
         gpu_usage = []
@@ -33,19 +43,52 @@ class GpuCollector(object):
         for i in range(self.gpu_num):
             gpu_handle = pynvml.nvmlDeviceGetHandleByIndex(i)
             gpu_percent = pynvml.nvmlDeviceGetUtilizationRates(gpu_handle).gpu
-            gpu_new_data = pd.DataFrame([{
+
+            new = pd.DataFrame([{
                 'Timestamp': timestamp,
-                'GPU Number': i,
+                'GPU': i,
                 'Percentage': gpu_percent
             }])
 
-            self.update_info(gpu_new_data)
+            self.update_gpu_util_data(new)
         
-        return self.data
+        return self.gpu_util_data
 
 
     @property
-    def get_color(self):
+    def gpu_process(self):
+        new_list = pd.DataFrame(columns=['GPU Memory (MB)', 'GPU', 'PID', 'User', 'Command'])
+
+        # Processes of the GPUs
+        for i in range(self.gpu_num):
+            gpu_handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            gpu_processes = pynvml.nvmlDeviceGetComputeRunningProcesses(gpu_handle)
+
+            for process in gpu_processes:
+                pid = process.pid
+                gpu_memory_usage = process.usedGpuMemory
+                process_info = psutil.Process(pid)
+                user = process_info.username()
+                command = ' '.join(process_info.cmdline())
+
+                new = pd.DataFrame([{
+                    'GPU Memory (MB)': gpu_memory_usage / 1024 / 1024,  # Convert to MB
+                    'GPU': i,
+                    'PID': str(pid),
+                    'User': user,
+                    'Command': command
+                }])
+
+                if not new.empty:
+                    new_list = pd.concat([new_list, new], ignore_index=True)
+
+        self.update_gpu_process_data(new_list)
+        
+        return self.gpu_process_data
+
+
+    @property
+    def color(self):
         return alt.Scale(
             domain=list(range(self.gpu_num)),
             range=[
